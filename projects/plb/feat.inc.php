dff715fd74fe4d716f4e6447f9b5ab275676d456
Ильяс <alesunix@outlook.com>
2020-03-20 14:54:18 +0600

    Create feat.inc.php

---
+++ projects/plb/feat.inc.php
@@ -0,0 +1,125 @@
+<?php
+
+# Данные о странице
+$title = 'Featuring';
+$keywords = 'Бишкек, Кыргызстан, Featuring';
+$description = 'Featuring';
+$template = 'info';
+$page_blocks = 'newsblock';
+
+# Содержание страницы
+$content = <<<EOF
+<style>
+.well {
+background: #fff; /* Цвет фона */
+}
+.fivti {
+    width: 100%;  
+    text-align: right;  	
+}
+</style>
+
+<div class='well'>
+Сборник совместных треков =Featuring=<br><br>
+<img src="/i/relizePLB/Featuring/Задняя_Лицевая.jpg" width=20% align=right>
+
+
+<audio controls style="width:100%;max-width:600px;">  
+    <source src="/audio/=Featuring=/=PLB= feat Mc Лёха - Футбол.mp3" type="audio/mp3">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat Mc Лёха - Футбол.mp3
+  <a href="/audio/=Featuring=/=PLB= feat Mc Лёха - Футбол.mp3" download="=PLB= feat Mc Лёха - Футбол.mp3">Скачать</a></p>
+  
+
+<audio controls style="width:100%;max-width:600px;">  
+    <source src="/audio/=Featuring=/=PLB= feat MT. -  Новогодняя.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat MT. -  Новогодняя.mp3
+  <a href="/audio/=Featuring=/=PLB= feat MT. -  Новогодняя.mp3" download="=PLB= feat MT. -  Новогодняя.mp3">Скачать</a></p>
+  
+ 
+<audio controls style="width:100%;max-width:600px;">  
+    <source src="/audio/=Featuring=/=PLB= feat Алтыша - Зеленая звезда.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat Алтыша - Зеленая звезда.mp3
+  <a href="/audio/=Featuring=/=PLB= feat Алтыша - Зеленая звезда.mp3" download="=PLB= feat Алтыша - Зеленая звезда.mp3">Скачать</a></p>
+  
+  
+<audio controls style="width:100%;max-width:600px;">  
+    <source src="/audio/=Featuring=/=PLB= feat Дифект речи - DISS.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat Дифект речи - DISS.mp3
+  <a href="/audio/=Featuring=/=PLB= feat Дифект речи - DISS.mp3" download="=PLB= feat Дифект речи - DISS.mp3">Скачать</a></p>
+  
+  
+<audio controls style="width:100%;max-width:600px;">  
+    <source src="/audio/=Featuring=/=PLB= feat Дифект речи - В прошлом.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat Дифект речи - В прошлом.mp3
+  <a href="/audio/=Featuring=/=PLB= feat Дифект речи - В прошлом.mp3" download="=PLB= feat Дифект речи - В прошлом.mp3">Скачать</a></p>
+  
+  
+<audio controls style="width:100%;max-width:600px;">  
+    <source src="/audio/=Featuring=/=PLB= feat Дифект речи - Микрофон.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat Дифект речи - Микрофон.mp3
+  <a href="/audio/=Featuring=/=PLB= feat Дифект речи - Микрофон.mp3" download="=PLB= feat Дифект речи - Микрофон.mp3">Скачать</a></p>
+  
+ 
+<audio controls style="width:100%;max-width:600px;">  
+    <source src="/audio/=Featuring=/=PLB= feat Клава - Белый минус (REMIX).mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+   <p>=PLB= feat Клава - Белый минус (REMIX).mp3
+  <a href="/audio/=Featuring=/=PLB= feat Клава - Белый минус (REMIX).mp3" download="=PLB= feat Клава - Белый минус (REMIX).mp3">Скачать</a></p>
+  
+  
+<audio controls style="width:100%;max-width:600px;">   
+    <source src="/audio/=Featuring=/=PLB= feat Клава - Мир на двоих (REMIX).mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat Клава - Мир на двоих (REMIX).mp3
+  <a href="/audio/=Featuring=/=PLB= feat Клава - Мир на двоих (REMIX).mp3" download="=PLB= feat Клава - Мир на двоих (REMIX).mp3">Скачать</a></p>
+  
+ 
+<audio controls style="width:100%;max-width:600px;">  
+    <source src="/audio/=Featuring=/=PLB= feat Ксюша - Мир на двоих (Рэмикс).mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+   <p>=PLB= feat Ксюша - Мир на двоих (Рэмикс).mp3
+  <a href="/audio/=Featuring=/=PLB= feat Ксюша - Мир на двоих (Рэмикс).mp3" download="=PLB= feat Ксюша - Мир на двоих (Рэмикс).mp3">Скачать</a></p>
+  
+  
+<audio controls style="width:100%;max-width:600px;">   
+    <source src="/audio/=Featuring=/=PLB= feat Ксюша - На будущее.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat Ксюша - На будущее.mp3
+  <a href="/audio/=Featuring=/=PLB= feat Ксюша - На будущее.mp3" download="=PLB= feat Ксюша - На будущее.mp3">Скачать</a></p>
+  
+  
+<audio controls style="width:100%;max-width:600px;">   
+    <source src="/audio/=Featuring=/=PLB= feat Общий знакомый - Общий знакомый.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat Общий знакомый - Общий знакомый.mp3
+  <a href="/audio/=Featuring=/=PLB= feat Общий знакомый - Общий знакомый.mp3" download="=PLB= feat Общий знакомый - Общий знакомый.mp3">Скачать</a></p>
+  
+  
+<audio controls style="width:100%;max-width:600px;">   
+    <source src="/audio/=Featuring=/=PLB= feat. Дифект речи - Микрофон.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером.  
+  </audio>
+  <p>=PLB= feat. Дифект речи - Микрофон(remix).mp3
+  <a href="/audio/=Featuring=/=PLB= feat. Дифект речи - Микрофон.mp3" download="=PLB= feat. Дифект речи - Микрофон.mp3">Скачать</a></p>
+
+</div>
+
+
+EOF;
