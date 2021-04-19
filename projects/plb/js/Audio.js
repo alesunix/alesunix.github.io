3e99243008f91d396d9966cb576767bc3100128b
Ильяс <alesunix@outlook.com>
2021-04-19 07:48:17 +0600

    up

---
+++ projects/plb/js/Audio.js
@@ -0,0 +1,22 @@
+//Audio HTML Запрет одновременного воспроизведения песен + последовательное воспроизведение.
+        let audioNL = document.querySelectorAll('audio');
+        let audio = Array.apply(null, audioNL);
+
+        audio.forEach(t => {
+            let index = audio.indexOf(t);
+
+            t.addEventListener('play', ()=> {
+                audio.forEach(subT => {
+                    subT !== audio[index] ?
+                    (subT.pause(), subT.currentTime = 0) :
+                    subT.play()
+                })
+            })
+            t.addEventListener('ended', ()=> {
+                t.currentTime = 0;
+                index !== audio.length - 1 ?
+                    audio[index + 1].play() :
+                    null
+            })
+        });
+        
